1f6b1a5ebc39ccad05c0761eb9027e7fe7025dbd
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2011-06-30 00:02:19 +0100

    added visibility functions

--- core/Kernel.js
+++ core/Kernel.js
@@ -10,6 +10,37 @@ define([
             dojo.style(elem, { display: (visible ? "block" : "none")});
         },
         
+        setWidgetVisible: function(widget, visible) {
+            var node = widget.domNode;
+            var parent = this.getParent(widget);
+            if(!visible) {
+                if(this.isWidgetVisible(widget)) { // hide if visible
+                    widget.__sumo_display = dojo.getComputedStyle(node).display;
+                    dojo.style(node, { display: "none"});
+                    if(parent)
+                        parent()
+                }
+            } else {
+                if(!this.isWidgetVisible(widget)) { // show if hidden
+                    var display = widget.__sumo_display || "block";
+                    dojo.style(node, { display: display });
+                }
+            }
+        },
+        
+        isWidgetVisible: function(widget) {
+            var cs = dojo.getComputedStyle(widget.domNode);
+            return cs.display != "none";
+        },
+        
+		getParent: function(widget) {
+			// summary:
+			//		Returns the parent widget of this widget, assuming the parent
+			//		specifies isContainer
+			var parent = dijit.getEnclosingWidget(widget.domNode.parentNode);
+			return parent && parent.isContainer ? parent : null;
+		},        
+        
         Horizontal: 1,
         Vertical: 2,
         
